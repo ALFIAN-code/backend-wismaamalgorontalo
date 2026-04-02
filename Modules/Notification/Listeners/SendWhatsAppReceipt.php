3c863d3fa3bccbbb4dce7e44f5a32d9c4f2b493d
Rizal Dinata <rizalldnt@gmail.com>
2026-04-02 14:11:16 +0700

    Feat(midtrans): add new module notification handleer dan event listener

---
+++ Modules/Notification/Listeners/SendWhatsAppReceipt.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Notification\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendWhatsAppReceipt
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct() {}
+
+    /**
+     * Handle the event.
+     */
+    public function handle($event): void {}
+}
